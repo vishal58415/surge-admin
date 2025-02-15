58af415d404f973aec2ae5c78ef275775b0cd1ba
Lalit Joshi <lalitjoshi379@gmail.com>
2025-02-15 13:45:38 +0000

    New Created Deals updated

--- src/controllers/AdminController.ts
+++ src/controllers/AdminController.ts
@@ -7,8 +7,64 @@ import config from "../configs/global";
 import moment from "moment";
 import { customLog } from "../lib/customLogger";
 import { logger } from "../lib/logger";
+import * as routesConstants from "../constants/routesConstants";
 
 console.log(" baseUrlenv " + process.env.NODE_ENV);
+
+export const deals_dashboard = ((req: any, res: Response) => {
+  const objResponse: any = {};
+  try {
+    objResponse['title'] = "New Created Deals";
+    const deals = [
+      {
+        "deal_name": "Deal 1",
+        "stage": "Conversation",
+        "deal_value": "$04,51,000",
+        "probability": "85%",
+        "status": "Lost"
+      },
+      {
+        "deal_name": "Deal 2",
+        "stage": "Pipeline",
+        "deal_value": "$14,51,000",
+        "probability": "56%",
+        "status": "Won"
+      },
+      {
+        "deal_name": "Deal 3",
+        "stage": "Won",
+        "deal_value": "$12,51,000",
+        "probability": "15%",
+        "status": "Won"
+      },
+      {
+        "deal_name": "Deal 4",
+        "stage": "Lost",
+        "deal_value": "$51,000",
+        "probability": "45%",
+        "status": "Lost"
+      },
+      {
+        "deal_name": "Wisozk",
+        "stage": "Follow Up",
+        "deal_value": "$67,000",
+        "probability": "5%",
+        "status": "Won"
+      }
+    ];
+
+    objResponse['newDeals'] = deals;
+
+  } catch (err) {
+    console.log("err" + JSON.stringify(err));
+  }
+  res.render(routesConstants.index, {
+    title: "Dashboard - CRMS admin template",
+    layout: routesConstants.index_layout,
+    page_path: "dashboard/deals-dashboard",
+    data: objResponse
+  });
+});
 
 export const createNewNote = async (
   req: any,
